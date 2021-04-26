fa40173dbcb95fae48d48a5b814fe2c00bce2785
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-04-26 13:46:37 +0100

    Fix bug where number of items loaded from API in App.js was inconsistent with logic in line 6 of LoadMore.js. Explore to centralise in single document for future releases to avoid inconsistencies

--- src/components/LoadMore.js
+++ src/components/LoadMore.js
@@ -3,7 +3,7 @@ import { Button } from "@chakra-ui/react";
 
 export default function LoadMore({ setPage, appMode }) {
   const loadMoreHandler = () => {
-    setPage((page) => ({ ...page, [appMode]: page[appMode] + 9 }));
+    setPage((page) => ({ ...page, [appMode]: page[appMode] + 12 }));
   };
   return (
     <Button m="2rem" bg="gray.500" onClick={loadMoreHandler}>
